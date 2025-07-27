dbb287e033f04ffb150550fa0e08ed0ba3701203
Muhammad Ma'ruf <maruuf82@gmail.com>
2025-07-27 18:51:42 +0700

    ubah logic notif telegram

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-'use client';
-
 import {
   AboutSection,
   ContactSection,
   ExperienceSection,
   HeroSection,
 } from '@/components/ui';
-import React, { useEffect } from 'react';
 
 const Home = () => {
-  useEffect(() => {
-    fetch('/api/info').then((res) => res.text);
-  }, []);
   return (
     <div className="flex flex-col items-center">
       <HeroSection />
