d980eaf2e86259f7202c4c94fa16dddf1f9c7169
Muhammad Ma'ruf <maruuf82@gmail.com>
2025-07-28 00:22:18 +0700

    desc under development,

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-import {
-  AboutSection,
-  ContactSection,
-  ExperienceSection,
-  HeroSection,
-} from '@/components/ui';
+import { AboutSection, HeroSection } from '@/components/ui';
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center">
       <HeroSection />
       <AboutSection />
-      <ExperienceSection />
-      <ContactSection />
+      {/* <ExperienceSection /> */}
+      {/* <ContactSection /> */}
     </div>
   );
 };
