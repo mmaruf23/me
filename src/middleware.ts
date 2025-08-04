83a48e1f8966696be52d468580956c7925a67ee4
Muhammad Ma'ruf <maruuf82@gmail.com>
2025-08-04 22:49:12 +0700

    error matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,6 +28,6 @@ export const middleware = (req: Request) => {
   return NextResponse.next();
 };
 
-// export const config = {
-//   matcher: '/',
-// };
+export const config = {
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+};
