b3ac0e1f3dda842ee6ff1ecd5a89e8733f22a250
Muhammad Ma'ruf <maruuf82@gmail.com>
2025-08-05 13:42:07 +0700

    bring back yukino.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import ContactInfo from '@/components/layouts/ContactInfo';
 import { Navbar } from '@/components/layouts/Navbar';
 import { AboutSection, HeroSection } from '@/components/ui';
+import type { Cookie } from '@/types/cookie';
+import type { GetServerSideProps } from 'next';
 import React from 'react';
 
 const Home = () => {
@@ -18,6 +20,35 @@ const Home = () => {
       </div>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const cookies: Cookie = req.cookies;
+
+  if (cookies.isReported !== 'true') {
+    await fetch(
+      `https://api.telegram.org/bot${process.env
+        .BOT_TOKEN_YUKINO!}/sendMessage`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          chat_id: process.env.CHAT_ID!,
+          text: 'Someone has accessed your portfolio!',
+        }),
+      }
+    );
+    res.setHeader(
+      'Set-Cookie',
+      `isReported=true; Path=/; HttpOnly; SameSite=Lax`
+    );
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default Home;
